63150f879a49e81dde6edc0c5253bf2226f6a2a2
mathison42 <mathison42@gmail.com>
2017-01-08 18:17:42 -0500

    Various Updates
    - New start parameter. Code will determine whether start time is
    specified in url or start parameter
    - Various checks to confirm input is valid
    - Added javascript comments
    - Updated README Directions

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
-# Gfycat Maker
+# Gfycat Gif Maker
 
 Simple web page to create gifs using Gfycat's API.
 
 ### Directions
-1. Specify a Youtube video at specific time.
+1. Open `simple.html` with your favorite browser.
 
-  `Example: https://youtu.be/SaVJyAwVUzY?t=49`
+2. Select Video (and start time)
+  1. Specify a Youtube video url with specific time.
 
-2. Decide length of video.
+  `Example URL with start time: https://youtu.be/SaVJyAwVUzY?t=49`
 
-  `Example: 6`
+  or
 
-3. Click "Create" to generate your new Gfycat gif
+  2. Specify any video url and start time (in seconds).
+
+    `Example URL without start time: https://youtu.be/SaVJyAwVUzY`
+
+    `Example start time: 49`
+
+  Note: The `start` parameter takes preference over the video's specified time.
+
+3. Specify the length (in seconds) of Gfycat gif.
+
+  `Example length/duration: 6`
+
+4. Click "Create" to generate your new Gfycat gif
 
   `Example: https://gfycat.com/SilverDiligentAruanas`
--- gfycat-functions.js
+++ gfycat-functions.js
@@ -1,22 +1,60 @@
 
+/**
+ * form Data containing url, start, and duration parameters.
+ * return Confirms input, calls the create HTTP POST call, and
+ *     updates page with location of new Gfycat Gif.
+ */
 function createGfycat(form) {
+
+  // Get variables from the form data
   var url = form.url.value;
+  var start = form.start.value;
   var duration = form.duration.value;
-  if (!url) {
-    url = "https://youtu.be/SaVJyAwVUzY?t=49"
+
+  // Various checking of the input variables
+  // URL
+  if (!checkURL(url)) {
+    alert("[Error - 'Video URL'] Please enter a valid URL. \n" +
+      "Examples:\n" +
+      "- https://www.youtube.com/watch?v=kv1IpEsHS3A\n" +
+      "- https://youtu.be/SaVJyAwVUzY?t=49");
+    return false
   }
-  if (!duration || duration <= 0) {
-    duration = 6
+
+  // Start
+  start = findStart(start, url);
+  if (!start) {
+    alert("[Error - 'Start'] Please enter number greater than 0 for the start parameter or provide a url with a start time.");
+    return false
   }
+
+  // Duration
+  if (!checkDuration(duration)) {
+    alert("[Error - 'Duration'] Please enter number greater than 0 for the duration parameter.");
+    return false
+  }
+
+  // Logging of input variables
   console.log("URL: " + url);
+  console.log("Start: " + start);
   console.log("Duration: " + duration);
-  callGfycatPost(url, duration);
+
+  // Create the gif
+  callCreateGfycat(url, start, duration);
+
   return false;
 
 }
 
-function callGfycatPost(url, duration) {
-  var start = getStartTime(url)
+/**
+ * url The url to fetch the video from.
+ * start Time to start the gif, in seconds.
+ * duration How long the gif will, be in seconds.
+ * result Calls post method to generate Gfycat gif. Updates html page with new link.
+ */
+function callCreateGfycat(url, start, duration) {
+
+  // Http Post's payload
   var data = JSON.stringify({
     "fetchUrl": url,
     "noMd5" : "true",
@@ -27,6 +65,8 @@ function callGfycatPost(url, duration) {
       }
   })
 
+  // jQuery Ajax call to make HTTP Post request
+  // https://api.jquery.com/jquery.post/
   $.ajax({
     type: "POST",
     beforeSend: function(request) {
@@ -36,24 +76,85 @@ function callGfycatPost(url, duration) {
     url: "https://api.gfycat.com/v1/gfycats",
     data: data,
     success: function(msg) {
-      console.log(msg)
+      // Once POST call returns, update the page
       updatePage(msg.gfyname)
     }
+  })
+  .done(function(msg) {
+    console.log("[Ok] callCreateGfycat:", msg);
+  })
+  .fail(function(msg) {
+    console.log("[Error] callCreateGfycat:", msg);
+    alert("[Error] Unable to create the Gfycat gif. Please try again.");
   });
 }
 
-function getStartTime(url) {
-  return url.split("=").pop()
-}
-
+/**
+ * gfyname The unique name given to the Gif by the Gfycat API.
+ * result Updates the HTML page with the Gfycat's gif name and link.
+ */
 function updatePage(gfyname) {
   var urlOutput = document.getElementById('gfycatResult');
   urlOutput.innerHTML = gfyname;
   urlOutput.setAttribute('href', 'https://gfycat.com/' + gfyname);
 }
 
+/**
+ * url Any type of url.
+ * result Retrieves the string following the last '=' character. If a youtube video,
+ *    this will be the start time.
+ */
+function getStartTime(url) {
+  return url.split("=").pop()
+}
 
-// function authenticateGfycat(url, duration) {
+/**
+ * url Any type of url.
+ * result Boolean determining validity of given url.
+ */
+function checkURL(url) {
+  // Regex Magic - Note: It's not perfect
+  // http://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url?page=1&tab=votes#tab-top
+  var pattern = new RegExp(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/);
+  if(!pattern.test(url)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/**
+ * start Specifed start time of video.
+ * url Any url to find start time if start == null.
+ * result Return the valid start time if found, otherwise false.
+ */
+function findStart(start, url) {
+  if (!start) {
+    start = getStartTime(url);
+  }
+  if (isNaN(start) || start < 0) {
+    return false;
+  } else {
+    return start;
+  }
+}
+
+/**
+ * duration The duration of the Gfycat gif, in seconds.
+ * result Boolean determining validity of given duration.
+ */
+function checkDuration(duration) {
+  if (isNaN(duration) || duration <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/**
+ * result Gfycat authentication secret returned (must specify client_id and client_secret)
+ */
+// function authenticateGfycat() {
 //   console.log("Authenticating Gfycat..")
 //   var data = {
 //     "grant_type": "client_credentials",
@@ -62,10 +163,13 @@ function updatePage(gfyname) {
 //   }
 //   $.post("https://api.gfycat.com/v1/oauth/token", JSON.stringify(data), function(data, status){
 //         console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-//         return callGfycatPost(url, duration, data.access_token)
 //     });
 // }
 
+/**
+ * prevColor Any color in Hex. Used to ensure repeat colors are not chosen.
+ * result Random 1 or 20 colors in Hex that is not the prevColor
+ */
 // function randomColor(prevColor) {
 //   var result;
 //   var colors = ["#ff0000","#ff4000","#ff8000","#ffbf00","#ffff00","#bfff00","#80ff00",
@@ -74,8 +178,8 @@ function updatePage(gfyname) {
 //     "#ff0080", "#ff0040", "#ff0000"];
 //
 //    do {
-//      var result = Math.floor(Math.random() * colors.size())
+//      result = Math.floor(Math.random() * colors.size())
 //    } while(result != prevColor);
 //
-//   return "#ff0000";
+//   return result;
 // }
